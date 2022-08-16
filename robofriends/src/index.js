fc3c47412236551d1b607260a3e6c725c6427a1f
Seaf Aliyan <codo47@gmail.com>
2022-08-16 13:08:40 +0300

    added h1 and search, no states yet

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,23 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Card from "./Card";
+import App from "./App";
 import "tachyons";
-import { robots } from "./robots";
+
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <div>
-    {robots.map((user, i) => {
-      return (
-        <Card
-          key={i}
-          id={robots[i].id}
-          name={robots[i].name}
-          username={robots[i].username}
-          email={robots[i].email}
-        />
-      );
-    })}
-  </div>
+  <App/>
 );
